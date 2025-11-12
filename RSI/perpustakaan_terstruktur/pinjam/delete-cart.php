bb13c6ec32859574da38fc7d7c1a9aa7289f69e9
dafaluthfan <dafaluthfan@student.ub.ac.id>
2025-11-12 23:02:32 +0700

    Delete Folder

--- RSI/perpustakaan_terstruktur/pinjam/delete-cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-function delete($i)
-{
-    $cookie_name = "cart";
-    $cart = json_decode($_COOKIE[$cookie_name], true);
-    array_splice($cart, $i, 1);
-    setcookie($cookie_name, json_encode($cart));
-}
-?>
